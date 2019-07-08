ba617a5f725dce36278c50168f1adba578e94a2b
Andrew Bowen <andrewbowen@dhcp-10-105-27-178.wireless.northwestern.private>
2019-07-08 14:28:40 -0500

    linear plot

--- python_plot.py
+++ python_plot.py
@@ -4,7 +4,8 @@ import matplotlib.pyplot as plt
 input_array = np.linspace(0, 2 * np.pi, 1000)
 
 sin_curve = np.cos(input_array)
-
+x = input_array
+y = x
 f,ax = plt.subplots(figsize = (8,5))
-ax.plot(input_array, sin_curve, c = '#99badd')
+ax.plot(x, y, c = '#99badd')
 plt.show()
