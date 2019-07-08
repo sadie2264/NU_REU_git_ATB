0e0b1db51140fb518e39a89153633c64644eb6a4
mercedes collins <mercedescollins@my.unt.edu>
2019-07-08 14:25:25 -0500

    not my code

--- python_plot.py
+++ python_plot.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 input_array = np.linspace(0, 2 * np.pi, 1000)
 
-sin_curve = np.cos(input_array)
+exp_plot = np.cos(input_array)
 
 f,ax = plt.subplots(figsize = (8,5))
 ax.plot(input_array, sin_curve, c = '#99badd')
